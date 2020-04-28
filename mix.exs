04227e163737397e0d08e47a54b87c5fa99f9312
josh rotenberg <joshrotenberg@gmail.com>
2020-04-27 21:06:59 -0700

    prep for 0.1.0 release

--- mix.exs
+++ mix.exs
@@ -6,6 +6,10 @@ defmodule Strsim.MixProject do
       app: :strsim,
       version: "0.1.0",
       elixir: "~> 1.10",
+      source_url: "https://github.com/joshrotenberg/strsim_ex",
+      test_coverage: [tool: ExCoveralls],
+      description: description(),
+      package: package(),
       start_permanent: Mix.env() == :prod,
       compilers: [:rustler] ++ Mix.compilers(),
       rustler_crates: [
@@ -14,6 +18,18 @@ defmodule Strsim.MixProject do
         ]
       ],
       deps: deps()
+    ]
+  end
+
+  defp description do
+    "An Elixir wrapper around the Rust strsim crate with rustler."
+  end
+
+  defp package do
+    [
+      maintainers: ["Josh Rotenberg"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/joshrotenberg/strsim_ex"}
     ]
   end
 
@@ -26,7 +42,10 @@ defmodule Strsim.MixProject do
   defp deps do
     [
       {:rustler, "~> 0.21.0"},
-      {:credo, "~> 1.4", only: [:dev, :test], runtime: false}
+      {:credo, "~> 1.4", only: [:dev, :test], runtime: false},
+      {:excoveralls, "~> 0.10", only: :test},
+      {:ex_doc, "~> 0.21", only: :dev, runtime: false}
+
     ]
   end
 end
