d96bf4fe4a04030e98e1f24b06f5cb738474ef75
josh rotenberg <joshrotenberg@gmail.com>
2020-04-27 20:50:25 -0700

    reorder

--- lib/strsim/nif.ex
+++ lib/strsim/nif.ex
@@ -1,13 +1,13 @@
 defmodule Strsim.Nif do
   use Rustler, otp_app: :strsim, crate: "strsim_nif"
 
-  def hamming(_a, _b), do: :erlang.nif_error(:nif_not_loaded)
-  def levenshtein(_a, _b), do: :erlang.nif_error(:nif_not_loaded)
   def damerau_levenshtein(_a, _b), do: :erlang.nif_error(:nif_not_loaded)
-  def osa_distance(_a, _b), do: :erlang.nif_error(:nif_not_loaded)
+  def hamming(_a, _b), do: :erlang.nif_error(:nif_not_loaded)
   def jaro(_a, _b), do: :erlang.nif_error(:nif_not_loaded)
   def jaro_winkler(_a, _b), do: :erlang.nif_error(:nif_not_loaded)
-  def normalized_levenshtein(_a, _b), do: :erlang.nif_error(:nif_not_loaded)
-  def sorensen_dice(_a, _b), do: :erlang.nif_error(:nif_not_loaded)
+  def levenshtein(_a, _b), do: :erlang.nif_error(:nif_not_loaded)
   def normalized_damerau_levenshtein(_a, _b), do: :erlang.nif_error(:nif_not_loaded)
+  def normalized_levenshtein(_a, _b), do: :erlang.nif_error(:nif_not_loaded)
+  def osa_distance(_a, _b), do: :erlang.nif_error(:nif_not_loaded)
+  def sorensen_dice(_a, _b), do: :erlang.nif_error(:nif_not_loaded)
 end
