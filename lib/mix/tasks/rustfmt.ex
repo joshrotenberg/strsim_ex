3ac3ce289852c8d39c81079a82bc149b92765966
josh rotenberg <joshrotenberg@gmail.com>
2020-04-27 20:52:38 -0700

    rustfmt mix task

--- lib/mix/tasks/rustfmt.ex
+++ lib/mix/tasks/rustfmt.ex
@@ -2,10 +2,13 @@ defmodule Mix.Tasks.Rustfmt do
   use Mix.Task
 
   @rs_path_wildcard "native/*/src/*.rs"
-  @shortdoc "Simply calls the Hello.say/0 function."
+
+  @shortdoc "Calls rustfmt on all .rs files found in the native src directory."
   def run(_) do
-    p = Path.expand(@rs_path_wildcard)
-    |> Path.wildcard()
+    p =
+      Path.expand(@rs_path_wildcard)
+      |> Path.wildcard()
+
     System.cmd("rustfmt", p)
   end
 end
