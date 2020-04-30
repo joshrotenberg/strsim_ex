7cb979939240ca383ac6f6b9083baf382a980279
josh rotenberg <joshrotenberg@gmail.com>
2020-04-29 19:10:12 -0700

    benchmark all implemented functions

---
+++ bench/strsim.exs
@@ -0,0 +1,24 @@
+Benchee.run(
+  %{
+    "strsim damerau_levenshtein" => fn -> Strsim.damerau_levenshtein("Dwayne", "Duane") end,
+    "strsim hamming" => fn -> Strsim.hamming("Dwayne", "Duane") end,
+    "strsim jaro" => fn -> Strsim.jaro("Dwayne", "Duane") end,
+    "strsim jaro_winkler" => fn -> Strsim.jaro_winkler("Dwayne", "Duane") end,
+    "strsim levenshtein" => fn -> Strsim.levenshtein("Dwayne", "Duane") end,
+    "strsim normalized_damerau_levensthein" => fn -> Strsim.normalized_damerau_levenshtein("Dwayne", "Duane") end,
+    "strsim normalized_levensthein" => fn -> Strsim.normalized_levenshtein("Dwayne", "Duane") end,
+    "strsim osa_distance" => fn -> Strsim.osa_distance("Dwayne", "Duane") end,
+    "strsim sorensen_dice" => fn -> Strsim.sorensen_dice("Dwayne", "Duane") end
+  },
+  formatters: [
+    {Benchee.Formatters.Markdown,
+    file: "bench/strsim_benchmark_results.md",
+    description: """
+    This benchmark compares all of the implemented strsim functions.
+    """
+  },
+    Benchee.Formatters.Console
+  ],
+  time: 10,
+  memory_time: 2
+)
